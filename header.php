9a0ddb70501a56331ba387e5b1c91dcebcc14cf4
Ananda54 <anandaaryal54@gmail.com>
2023-04-25 12:47:00 +0545

    commit 1

--- header.php
+++ header.php
@@ -8,17 +8,12 @@
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
       rel="stylesheet"
     />
+    <link rel="stylesheet" href="assets/css/header.css">
     <title>Deli Nepal</title>
     <link rel="icon" type="images/ico" href="favicon.ico">
     <?php include "base_url.php"; ?>
   </head>
-  <style>
-    body{
-        overflow: hidden;
-    }
-  </style>
   <body>
-
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
             <a class="navbar-brand" href="<?php echo BASE_URL; ?>">
@@ -27,13 +22,7 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#"><img src="<?php echo BASE_URL; ?>assets/image/b2b.png" class="w-50"></a>
-                    </li>
-                </ul>
-            </div>
+            <a class="nav-link" aria-current="page" href="<?php echo BASE_URL; ?>vendorlogin"><img src="<?php echo BASE_URL; ?>assets/image/b2b.png" class="w-50"></a>
         </div>
     </nav>
 
