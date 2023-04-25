d450918bbe62f855009face0c0765a50fbdb0d74
chrazesh <chrazesh3@gmail.cpm>
2023-04-25 16:18:19 +0545

    rajesh 2023/04/25 commit 11

--- header.php
+++ header.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-    />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- bootstrap icon cdn -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+  
+ 
     <link rel="stylesheet" href="assets/css/header.css">
     <title>Deli Nepal</title>
     <link rel="icon" type="images/ico" href="favicon.ico">
