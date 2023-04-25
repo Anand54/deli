bace1948dadef611093ef0ebbd7e8b8775698155
Ananda54 <anandaaryal54@gmail.com>
2023-04-25 11:24:19 +0545

    commit 1

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#"></a>
+                        <a class="nav-link" aria-current="page" href="#"><img src="<?php echo BASE_URL; ?>assets/image/b2b.png" class="w-50"></a>
                     </li>
                 </ul>
             </div>
