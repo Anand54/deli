a29049a340b6fdd614beee04ca647717f984883a
Ananda54 <anandaaryal54@gmail.com>
2023-04-25 11:09:39 +0545

    commit 1

---
+++ banner.php
@@ -0,0 +1,3 @@
+<div class="row">
+    <div><img src="<?php echo BASE_URL?>assets/image/banner/2-Recovered.jpg" class="w-100"></div>
+</div>
---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <title>Deli Nepal</title>
+    <link rel="icon" type="images/ico" href="favicon.ico">
+    <?php include "base_url.php"; ?>
+  </head>
+  <style>
+    body{
+        overflow: hidden;
+    }
+  </style>
+  <body>
+
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="<?php echo BASE_URL; ?>">
+                <img class="logoImg ms-3 w-50" src="<?php echo BASE_URL; ?>assets/image/logo/deli-logo.png">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#"></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    
+
+
+
+    
