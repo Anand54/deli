0753858a787fc11fd6ceac1d8b192eb7383fa86a
chrazesh <chrazesh3@gmail.cpm>
2023-04-25 15:03:08 +0545

    rajesh 2023/04/25 commit 7

--- vendorRegister/index.php
+++ vendorRegister/index.php
@@ -1,8 +1,13 @@
+ <?php
+   include '../header.php'
+  ?>
+ 
+ 
  <form action="#" method="post" class="mb-2">
     <div class="row row-cols-1 row-cols-md-2">
       <div class="col">
         <div class="col-12 text-center">
-          <img src="http://delinepal.com/assets/images/Account/54483-vender-por-redes.gif" class="w-75"><br>
+          <img src="<?php echo BASE_URL; ?>assets/image/vendorRegister/54483-vender-por-redes.gif" class="w-75"><br>
         </div>
       </div>
       <div class="col">
@@ -17,14 +22,7 @@
             </div>
                 <div id="reggap">
             <div class=" row    " id="registermain">
-                <!--<div class="col form-floating   ">-->
-                <!--    <select class="form-select" id="selectType" aria-label="Default select example">-->
-                <!--      <option class="selectOption">--SELECT--</option>-->
-                <!--      <option value="Dealer">Dealer</option>-->
-                <!--      <option value="Wholesaler">Wholesaler</option>-->
-                <!--      <option value="Retailer">Retailer</option>-->
-                <!--    </select>   -->
-                <!--</div>-->
+             
             <div class="col form-floating   ">
               <input type="text" class="form-control ps-4" name="companyname" id="companyname" placeholder="Company">
               <label class="ms-4" for="floatingInput">Company</label>
@@ -56,22 +54,7 @@
               <label class="ms-4" for="floatingPassword">Password</label>
             </div>
             <div class="col form-floating">
-                <select class="form-select" id="province" aria-label="Default select example">
-                    <?php
-                                        $query = "SELECT DISTINCT `province` FROM `address`;";
-                                        // $conn = connectDB();
-                                        $req = mysqli_query($conn, $query) or die(mysqli_error($conn));
-                                        if (mysqli_num_rows($req) > 0) {
-                                            while ($data = mysqli_fetch_assoc($req)) {
-                                                $province = $data["province"]; ?>
-                                        <option value="<?Php echo $province; ?>">
-                                            <?Php echo $province; ?>
-                                        </option>
-                                        <?php
-                                            }
-                                        } ?>
-                </select> 
-                <label class="ms-4" for="province">Province</label>
+                                <label class="ms-4" for="province">Province</label>
                 </div>
             </div>
             <div class=" row  " id="registermain">
@@ -91,7 +74,7 @@
                 style="background:red; color:white">Request</button>
             </div>
             <div class=" mt-3 pb-3" id="registermain">
-              <span class="">Already have account ? <a href="../vendor_login/">Login</a></span>
+              <span class="">Already have account ? <a href="<?php echo BASE_URL; ?>vendorlogin/index.php/">Login</a></span>
             </div>
           </div>
           </div>
@@ -99,3 +82,7 @@
       </div>
     </div>
   </form>
+<?php
+   include '../footer.php'
+  ?>
+ 
