fbcf66e1efcc3ae650c24cfd7af01e3b8aff70e8
chrazesh <chrazesh3@gmail.com>
2023-04-26 15:26:11 +0545

    rajesh 2023/04/26 commit

--- vendor/order_history.php
+++ vendor/order_history.php
@@ -13,29 +13,15 @@
     </tr>
   </thead>
     <tbody>
-    <?php
-        $orderConfirm='';
-        $checkoutID = '';
-        $vendorEmail = $_SESSION['vendor_email'];
-        $query_checkoutQry = "SELECT  `id` as checkoutID,`order_confirmed`,`product_count`, `order_place_date` FROM `vendor_checkout` WHERE `vendor_email` ='$vendorEmail' ORDER BY `id` DESC;";
-        $conn = dbConnecting();
-        $req_checkout = mysqli_query($conn, $query_checkoutQry) or die(mysqli_error($conn));
-        if (mysqli_num_rows($req_checkout) > 0) {
-            $i = 1;
-            while ($data_show = mysqli_fetch_assoc($req_checkout)) {
-                 $checkoutID = $data_show['checkoutID'];
-                 $orderConfirm = $data_show['order_confirmed'];
-    ?>
+    
     <tr>
-      <td><?php echo $i; ?></td>
-      <td><?php echo $data_show['order_place_date'] ?></td>
-      <td><?php echo $data_show['product_count'] ?></td>
+      <td>1</td>
+      <td>32</td>
+      <td>3</td>
       <td class="col-2">
-          <a href="#" style="text-decoration:none; color:black;" data-checkoutID="<?php echo $data_show['checkoutID']; ?>"><i class="bi bi-eye toggleBtn"></i></a>
-      <?php if($orderConfirm=="0"){echo'<a class="ms-2 deleteBtn" data-checkouTID="'.$data_show['checkoutID'].'" herf="#" ><i class="bi bi-trash2-fill text-danger"></i></a>';}
-      else if($orderConfirm=="1"){}
-      ?>
-      <a href="vendor_invoice.php?ref=<?php echo $vendorEmail; ?>& id=<?php echo $checkoutID; ?>" class="printClass" target="blank"><i class="bi bi-printer-fill"></i></a>
+          <a href="#" style="text-decoration:none; color:black;"><i class="bi bi-eye toggleBtn"></i></a>
+       <a class="ms-2 deleteBtn" herf="#" ><i class="bi bi-trash2-fill text-danger"></i></a>
+      <a href="vendor_invoice.php"  class="printClass" target="blank"><i class="bi bi-printer-fill"></i></a>
       </td>
     </tr>
     <tr class="childTable">
@@ -51,57 +37,35 @@
                   <th>Price</th>
                   <th >Quantity</th>
                   <th >Amount</th>
-                  <th >Discount(<?php echo $discountPercent; ?>%)</th>
+                  <th >Discount(20%)</th>
                   <th >Total</th>
                   <th >Image</th>
                 </tr>
               </thead>
                 <tbody>
-                    <?php 
-                         $query = "SELECT  `vendor_checkout_items`.`id`,`category_name`,`product_code`,`product_name`, `vendor_checkout_id`,`product_id`, `order_quantity`,`img_path`, `primary_image`, `sales_rate`, `discountAmount`,`total_after_discount`, `total_amt` FROM `vendor_checkout_items`
-                                    INNER JOIN products on products.id = vendor_checkout_items.product_id 
-                                    INNER JOIN category ON category.category_id = products.category_id
-                                    WHERE `vendor_checkout_id` ='$checkoutID';";
-                         $conn = dbConnecting();
-                        $req = mysqli_query($conn, $query) or die(mysqli_error($conn));
-                        $sumTotal_o=0.0;
-                        if (mysqli_num_rows($req) > 0) {
-                            $j = 1;
-                            while ($data = mysqli_fetch_assoc($req)) {
-                    ?>
+                 
                 <tr>
-                  <td><?php echo $j; ?></td>
-                  <td><?php echo $data['category_name']; ?></td>
-                  <td><?php echo $data['product_code']; ?></td>
-                  <td><?php echo $data['product_name']; ?></td>
-                  <td><?php echo intval($data['sales_rate']); ?></td>
-                  <td><?php echo $data['order_quantity']; ?></td>
-                  <td><?php echo intval($data['total_amt']); ?></td>
-                  <td><?php echo intval($data['discountAmount']); ?></td>
-                  <td class="totalAfterDis"><?php echo intval($data['total_after_discount']); ?></td>
-                  <td class="col-1"><img src="<?php echo '../../'.$data['img_path'].$data['primary_image'];  ?>" class="w-100"></td>
+                  <td>1</td>
+                  <td>anand</td>
+                  <td>1223</td>
+                  <td>kjgjh</td>
+                  <td>sssss</td>
+                  <td>qqq</td>
+                  <td>wwwww</td>
+                  <td>jkjkkkk</td>
+                  <td class="totalAfterDis">hhjh</td>
+                  <td class="col-1"><img src="sssss" class="w-100"></td>
                 </tr>
-                    <?php
-                    $sumTotal_o=$sumTotal_o+intval($data['total_after_discount']);
-                    $j++;
-                        }
-                        }
-                    ?>
+                   
                 <tr class="fw-bold">
                     <td  colspan="8">Total</td>
-                    <td Id="totalAfterDiscount">Rs. <?php echo $sumTotal_o ?></td>
+                    <td Id="totalAfterDiscount">Rs. 20</td>
                 </tr>
               </tbody>
             </table> 
             </td>
       </tr>
-    <?php 
-    $i++;
-            }
-            }
-
-
-?>
+   
   </tbody>
 </table>
 </div>
@@ -115,7 +79,7 @@ $(document).ready(function(){
     
     $(".deleteBtn").click(function(){
       var checkoutID = $(this).attr('data-checkouTID');
-      var vendorEmail = "<?php echo $_SESSION['vendor_email'] ?>";
+      var vendorEmail = "anand@gmail.com";
      if(confirm("Are you sure you want to delete order list ?")){
      if(checkoutID==""||vendorEmail==""){
          alert("facing problem");
--- vendor/purchase_history.php
+++ vendor/purchase_history.php
@@ -13,27 +13,12 @@
     </tr>
   </thead>
     <tbody>
-    <?php
-        $orderConfirm='';
-        $checkoutID = '';
-        $vendorEmail = $_SESSION['vendor_email'];
-        $query_checkoutQry = "SELECT  `id` as checkoutID,`order_confirmed`,process_completed,`product_count`, `order_place_date` FROM `vendor_checkout` WHERE `vendor_email` ='$vendorEmail' AND `process_completed`='1';";
-        $conn = dbConnecting();
-        $req_checkout = mysqli_query($conn, $query_checkoutQry) or die(mysqli_error($conn));
-        if (mysqli_num_rows($req_checkout) > 0) {
-            $i = 1;
-            while ($data_show = mysqli_fetch_assoc($req_checkout)) {
-                 $checkoutID = $data_show['checkoutID'];
-                 $orderConfirm = $data_show['order_confirmed'];
-    ?>
+   
     <tr>
-      <td><?php echo $i; ?></td>
-      <td><?php echo $data_show['order_place_date'] ?></td>
-      <td><?php echo $data_show['product_count'] ?></td>
-      <td><a href="#" style="text-decoration:none; color:black;" data-checkoutID="<?php echo $data_show['checkoutID']; ?>"><i class="bi bi-eye toggleBtn"></i></a><?php if($orderConfirm=="0"){echo'<a class="ms-2 deleteBtn" data-checkouTID="'.$data_show['checkoutID'].'" herf="#" ><i class="bi bi-trash2-fill text-danger"></i></a>';}
-      else if($orderConfirm=="1"){}
-      ?>
-      <a href="vendor_invoice.php?ref=<?php echo $vendorEmail; ?>& id=<?php echo $checkoutID; ?>" class="printClass" target="blank"><i class="bi bi-printer-fill"></i></a>
+      <td>aaaa</td>
+      <td>bbbbbbbbbbbbb</td>
+      <td>ccccc</td>
+      <td>ddddd
       </td>
     </tr>
     <tr class="childTable">
@@ -49,54 +34,38 @@
                   <th>Price</th>
                   <th >Quantity</th>
                   <th >Amount</th>
-                  <th >Discount(<?php echo $discountPercent; ?>%)</th>
+                  <th >Discount(20%)</th>
                   <th >Total</th>
                   <th >Image</th>
                 </tr>
               </thead>
                 <tbody>
-                    <?php 
-                         $query = "SELECT  `vendor_checkout_items`.`id`,`category_name`, `product_code`,`product_name`,`discountAmount`,`vendor_checkout_id`,`product_id`, `order_quantity`,`img_path`, `primary_image`, `sales_rate`,`total_after_discount`,`total_amt` FROM `vendor_checkout_items`
-                                    INNER JOIN products on products.id = vendor_checkout_items.product_id 
-                                    INNER JOIN category ON category.category_id = products.category_id WHERE `vendor_checkout_id` ='$checkoutID';";
-                         $conn = dbConnecting();
-                        $req = mysqli_query($conn, $query) or die(mysqli_error($conn));
-                        $sumTotal=0.0;
-                        if (mysqli_num_rows($req) > 0) {
-                            $j = 1;
-                            while ($data = mysqli_fetch_assoc($req)) {
-                    ?>
+                  
                 <tr>
-                  <td><?php echo $j; ?></td>
-                  <td><?php echo $data['category_name']; ?></td>
-                  <td><?php echo $data['product_code']; ?></td>
-                  <td><?php echo $data['product_name']; ?></td>
-                  <td><?php echo intval($data['sales_rate']); ?></td>
-                  <td><?php echo $data['order_quantity']; ?></td>
-                  <td><?php echo intval($data['total_amt']); ?></td>
-                  <td><?php echo intval($data['discountAmount']); ?></td>
-                  <td class="totalAfterDis"><?php echo intval($data['total_after_discount']); ?></td>
-                  <td class="col-1"><img src="<?php echo '../../'.$data['img_path'].$data['primary_image'];  ?>" class="w-100"></td>
+                  <td>a</td>
+                  <td>bbbb</td>
+                  <td>ccc</td>
+                  <td>ddddd</td>
+                  <td>eeeeee</td>
+                  <td>ffff</td>
+                  <td>ggggg</td>
+                  <td>hhhh</td>
+                  <td>iiiii</td>
+                  <td>jjjjjj</td>
                 </tr>
-                    <?php
-                    $sumTotal=$sumTotal+intval($data['total_after_discount']);
-                    $j++;
-                        }
-                        }
-                    ?>
+                  
                      <tr class="fw-bold">
                     <td  colspan="8">Total</td>
-                    <td Id="totalAfterDiscount">Rs. <?php echo $sumTotal ?></td>
+                    <td Id="totalAfterDiscount">Rs.20</td>
                 </tr>
               </tbody>
             </table> 
             </td>
       </tr>
-    <?php 
-    $i++;
-            }
-            }
-?>
+     
+
+            
+        
   </tbody>
 </table>
 </div>
@@ -111,7 +80,7 @@ $(document).ready(function(){
     
     $(".deleteBtn").click(function(){
       var checkoutID = $(this).attr('data-checkouTID');
-      var vendorEmail = "<?php echo $_SESSION['vendor_email'] ?>";
+      var vendorEmail = "anand@gmail.com";
      if(confirm("Are you sure you want to delete order list ?")){
      if(checkoutID==""||vendorEmail==""){
          alert("facing problem");
